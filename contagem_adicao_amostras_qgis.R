e0cf9e31d9bea0b4485ca1d9f7b4f996ea0ba453
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2025-09-02 01:49:40 -0300

    Add files via upload

---
+++ contagem_adicao_amostras_qgis.R
@@ -0,0 +1,44 @@
+
+library(sf)
+
+# Tile 035016
+
+amostras_originais <- sf::read_sf("amostras_originais_tile_035016.shp")
+
+amostras_totais <- sf::read_sf("amostras_adicionais_035016.shp")
+
+View(amostras_originais)
+View(amostras_totais)
+
+# Extrair coordenadas e renomear
+totais_df <- amostras_totais %>%
+  mutate(longitude = st_coordinates(.)[,1],
+         latitude  = st_coordinates(.)[,2]) %>%
+  st_set_geometry(NULL)  # remove a geometria para virar data.frame
+
+originais_df <- amostras_originais %>%
+  mutate(longitude = st_coordinates(.)[,1],
+         latitude  = st_coordinates(.)[,2]) %>%
+  st_set_geometry(NULL)
+
+pontos_novos_df <- anti_join(totais_df, originais_df, by = c("longitude", "latitude"))
+
+View(pontos_novos_df)
+
+unique(pontos_novos_df$label)
+
+pontos_novos_df_ar <- pontos_novos_df |>
+  filter(label == "aflor_rocha")
+
+View(pontos_novos_df_ar)
+  
+  
+pontos_novos_df_sup <- pontos_novos_df |>
+  filter(label == "supressao")
+  
+View(pontos_novos_df_sup)
+  
+pontos_novos_df_agua <- pontos_novos_df |>
+  filter(label == "agua")
+
+View(pontos_novos_df_agua)

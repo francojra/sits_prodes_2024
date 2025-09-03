89283f44232cc4acd3b38e182ed0ff0b61e1a573
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2025-09-02 23:54:37 -0300

    Add files via upload

--- contagem_adicao_amostras_qgis.R
+++ contagem_adicao_amostras_qgis.R
@@ -27,11 +27,15 @@ originais_df <- amostras_originais %>%
          latitude  = st_coordinates(.)[,2]) %>%
   st_set_geometry(NULL)
 
+# Verificar as amostras que não estão contidas nas duas tabelas
+
 pontos_novos_df <- anti_join(totais_df, originais_df, by = c("longitude", "latitude"))
 
 View(pontos_novos_df)
 
 unique(pontos_novos_df$label)
+
+# Verificar novas amostras adicionadas por classes
 
 pontos_novos_df_ar <- pontos_novos_df |>
   filter(label == "aflor_rocha")
